c8bd1b8674992f8a59d90530be2ccff83c88ca3f
Jok98 <jokmoi98@outlook.com>
2023-10-15 21:41:49 +0200

    UPDATED FRONTEND CODE

--- Backend/src/main/java/com/project/ProjectAlt96/ProjectAlt96Application.java
+++ Backend/src/main/java/com/project/ProjectAlt96/ProjectAlt96Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
+
 
 @SpringBootApplication
 public class ProjectAlt96Application {
--- Backend/src/main/java/com/project/ProjectAlt96/controller/LogInController.java
+++ Backend/src/main/java/com/project/ProjectAlt96/controller/LogInController.java
@@ -2,9 +2,6 @@ package com.project.ProjectAlt96.controller;
 
 import com.project.ProjectAlt96.model.dao.UsersDao;
 import com.project.ProjectAlt96.model.pojo.Users;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +9,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
 public class LogInController {
     @Autowired
     private UsersDao usersDao;
+
+    @PostMapping(path = "/save")
+    public ResponseEntity<String>  saveUser(@RequestBody Users user)
+    {
+
+        //Blocca l'operazione se password o email non sono presenti
+        if (user.getPassword().equals("") || user.getEmail().equals("")){
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        usersDao.save(user);
+        return ResponseEntity.ok("Utente registrato con successo!");
+
+
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<String>  loginUser(@RequestBody Users user)
+    {
+        if(user == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+
+        Users use = usersDao.getByUsername(user.getUsername());
+        if(use != null && use.getPassword().equals(user.getPassword()) && use.getUsername().equals(user.getUsername())){
+
+        return ResponseEntity.ok("Utente Loggato con successo!");}
+        else{  return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Utente non trovato");}
+
+    }
+
+
 
     @GetMapping("/loginApp")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
