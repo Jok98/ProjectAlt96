c1a3f382326eeeb6d24e2508a993ca76ee531782
Jok98 <matteo.moi1998@gmail.com>
2023-12-09 12:42:37 +0100

    implemented apis method in Expenses ms

--- Backend/Expenses/src/main/java/com/projectalt96/expenses/connector/FeignClientConnectorInterface.java
+++ Backend/Expenses/src/main/java/com/projectalt96/expenses/connector/FeignClientConnectorInterface.java
@@ -1,13 +1,22 @@
 package com.projectalt96.expenses.connector;
 
+import com.projectalt96.expenses.dto.ExpenseDTO;
 import com.projectalt96.expenses.dto.RetrieveExpensesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "core-service") // is the name of the service we want to connect to, registered in Eureka
 public interface FeignClientConnectorInterface {
-
     @GetMapping("/expense")
     ResponseEntity<RetrieveExpensesResponse> getExpensesList();
+
+    @DeleteMapping("/expense/{id}")
+    ResponseEntity<Boolean> deleteExpenseById(String id);
+
+    @GetMapping("/expense/{id}")
+    ResponseEntity<ExpenseDTO> getExpenseById(String id);
+
+    @PostMapping("/expense")
+    ResponseEntity<Boolean> createExpense(@RequestBody ExpenseDTO expenseDTO);
 }
--- Backend/Expenses/src/main/java/com/projectalt96/expenses/service/ExpensesService.java
+++ Backend/Expenses/src/main/java/com/projectalt96/expenses/service/ExpensesService.java
@@ -1,6 +1,7 @@
 package com.projectalt96.expenses.service;
 
 import com.projectalt96.expenses.connector.FeignClientConnectorInterface;
+import com.projectalt96.expenses.dto.ExpenseDTO;
 import com.projectalt96.expenses.dto.RetrieveExpensesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,23 @@ public class ExpensesService {
     FeignClientConnectorInterface feignClientConnectorInterface;
 
     public ResponseEntity<RetrieveExpensesResponse> getExpensesList() {
-        ResponseEntity<RetrieveExpensesResponse> result = feignClientConnectorInterface.getExpensesList();
-        result.getBody().getExpenses().forEach(System.out::println);
-        return result;
+        ResponseEntity<RetrieveExpensesResponse> response = feignClientConnectorInterface.getExpensesList();
+        response.getBody().getExpenses().forEach(System.out::println);
+        return response;
+    }
+
+    public ResponseEntity<Boolean> deleteExpenseById(String id) {
+        ResponseEntity<Boolean> response = feignClientConnectorInterface.deleteExpenseById(id);
+        return response;
+    }
+
+    public ResponseEntity<ExpenseDTO> getExpenseById(String id) {
+        ResponseEntity<ExpenseDTO> response = feignClientConnectorInterface.getExpenseById(id);
+        return response;
+    }
+
+    public ResponseEntity<Boolean> saveExpense(ExpenseDTO expenseDTO) {
+        ResponseEntity<Boolean> response = feignClientConnectorInterface.createExpense(expenseDTO);
+        return response;
     }
 }
